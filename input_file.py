f2f89d19281f51a1e0ab4ecd15e415f9df04ab36
Conor O'Kelly <cokelly@dhcp-c101a695.ucd.ie>
2016-02-25 15:11:57 +0000

    Updated gitignore and added install file

---
+++ input_file.py
@@ -0,0 +1,3 @@
+# Author = "Conor O'Kelly"
+
+# This python file should take the input file and convert it into an data strcture to be used in the installer
--- installer.py
+++ installer.py
@@ -1,21 +1,26 @@
 # Author = "Conor O'Kelly"
 import urllib.request
+import os
 
 # Main file to call all of the installer links
-def main():
+def main(user_name):
 
     down_links = ["vlc.dmg","http://get.videolan.org/vlc/2.2.2/macosx/vlc-2.2.2.dmg"]
 
-    donwload(down_links[0],down_links[1])
+    donwload(down_links[0],down_links[1],user_name)
+
+    os.mknod("downloads")
 
 
+def donwload(file_name, url,user_name):
 
-def donwload(file_name, url):
+    user_desktop = ("/Users/" + user_name + "/Desktop")
+    download_file = os.path.join(user_desktop, file_name)
+    urllib.request.urlretrieve(url, download_file)
 
-    urllib.request.urlretrieve(url, file_name)
     print("%s has finished downloading" % (file_name))
 
 
 if __name__ == '__main__':
     print("Now downloading all files and saving to desktop")
-    main()
+    main("cokelly")
